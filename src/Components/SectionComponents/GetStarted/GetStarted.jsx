e0a949428e9aa787acb627ddce73b2c9a489f230
Tamanna Tahsin <138390753+TamannaTahsin1@users.noreply.github.com>
2024-12-11 20:50:10 +0600

    footer section done

--- src/Components/SectionComponents/GetStarted/GetStarted.jsx
+++ src/Components/SectionComponents/GetStarted/GetStarted.jsx
@@ -2,11 +2,12 @@ import Button from "../../SharedComponents/Button/Button";
 import Container from "../../SharedComponents/Container/Container";
 import Title from "../../SharedComponents/Title/Title";
 import postizImg from "../../../assets/getStarted/postiz.png";
+
 const GetStarted = () => {
   return (
     <div className="rounded-xl bg-secondary">
       <Container>
-        <div className="space-y-5 px-5 pt-24 text-center ">
+        <div className="space-y-5 px-5 lg:py-24 text-center ">
           <Title
             className={"text-5xl font-bold"}
             text={"Ready to get started?"}
@@ -18,8 +19,8 @@ const GetStarted = () => {
           <div className="flex justify-center">
             <Button>{`Get Started >>`}</Button>
           </div>
-          <div>
-            <img src={postizImg} className="mx-auto " alt="" />
+          <div className="relative pt-32 lg:pt-64">
+            <img src={postizImg} className="absolute top-8 lg:top-1 mx-auto md:left-0 md:right-0" alt="" />
           </div>
         </div>
       </Container>
--- src/Components/SharedComponents/Footer/Footer.jsx
+++ src/Components/SharedComponents/Footer/Footer.jsx
@@ -1,11 +1,79 @@
-
+import logo from "../../../assets/Logo/logo.png";
+import Container from "../Container/Container";
+import { FaGithub, FaDiscord, FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import perrermint from "../../../assets/footer/peppermint.svg";
 
 const Footer = () => {
-    return (
-        <div>
-            
+  return (
+    <Container>
+      <div className="relative z-20 bg-black px-10 pt-14">
+        <div className=" grid  gap-3 space-y-8 text-center md:grid-cols-2 md:text-start lg:grid-cols-4">
+          <div className="space-y-4 md:pt-7">
+           <div className="ml-28 block md:mx-0">
+           <img src={logo}  className="mx-auto block md:mx-0" alt="" />
+           </div>
+            <p className="text-sm text-gray-400">
+              Open-source social media scheduling tool
+            </p>
+            <div className="hidden justify-start gap-3 md:flex">
+              <FaGithub className="size-8 rounded-full bg-[#3E3E3E] p-2" />
+              <FaDiscord className="size-8 rounded-full bg-[#3E3E3E] p-2" />
+              <FaLinkedinIn className="size-8 rounded-full bg-[#3E3E3E] p-2" />
+              <FaXTwitter className="size-8 rounded-full bg-[#3E3E3E] p-2" />
+            </div>
+          </div>
+          <div className="space-y-4">
+            <h1 className="font-semibold">Free Tools</h1>
+            <div className="space-y-2 text-sm text-gray-400">
+              <p>Free Marketing Tools</p>
+              <p>List your agency</p>
+            </div>
+          </div>
+          <div className="space-y-4">
+            <h1 className="font-semibold">Resources</h1>
+            <div className="space-y-2 text-sm text-gray-400">
+              <p>Blog</p>
+              <p>Docs</p>
+              <p>Channels</p>
+              <p>Roadmap</p>
+              <p>Discord</p>
+              <p>Alternatives</p>
+              <p>Comparisons </p>
+            </div>
+          </div>
+          <div className="space-y-4">
+            <h1 className="font-semibold">Company</h1>
+            <div className="space-y-2 text-sm text-gray-400">
+              <p>Pricing</p>
+              <p>Terms of service</p>
+              <p>Privacy Policy</p>
+            </div>
+          </div>
         </div>
-    );
+        <div className="my-10">
+          <hr className="border-[#1b1b1b]" />
+        </div>
+        <div className="space-y-4">
+          <div className="flex justify-center gap-3 md:hidden">
+            <FaGithub className="size-8 rounded-full bg-[#3E3E3E] p-2" />
+            <FaDiscord className="size-8 rounded-full bg-[#3E3E3E] p-2" />
+            <FaLinkedinIn className="size-8 rounded-full bg-[#3E3E3E] p-2" />
+            <FaXTwitter className="size-8 rounded-full bg-[#3E3E3E] p-2" />
+          </div>
+          <div className="flex flex-col-reverse gap-2 pb-6 text-center text-sm text-gray-300 md:flex-row md:justify-start md:gap-10">
+            <p>© Postiz, 2024. All rights reserved.</p>
+            <p className="flex items-center justify-center gap-2">
+              Designed by
+              <span>
+                <img src={perrermint} alt="" />
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Footer;
